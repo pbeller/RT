5d761ebc58c934dde832995328473022c7ba26ed
nmuller <nmuller@student.42.fr>
2018-04-14 20:10:08 +0200

    config.mk for macos

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 CONFIG_NAME = rt
 CONFIG_OUTPUT_TYPE = exec
 CONFIG_RUN_CMD = ./$(CONFIG_NAME)
-CONFIG_CC_FLAGS_EXTRA = -I minilibx
-CONFIG_CC_FLAGS_EXTRA_POST = -L ./minilibx -l mlx -lXext -lX11 -lm -lpthread -fsanitize=address
+CONFIG_CC_FLAGS_EXTRA = -I minilibx_macos
+CONFIG_CC_FLAGS_EXTRA_POST = -framework OpenGL -framework AppKit minilibx_macos/libmlx.a -lpthread -fsanitize=address
